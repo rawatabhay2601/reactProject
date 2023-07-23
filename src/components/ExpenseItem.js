924e2109318ac86aa558aff33549c9fe9828913e
rawatabhay2601 <rawatabhay2601@gmail.com>
2023-07-23 21:06:47 +0530

    adding event listeners for deleting using DOM

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -4,15 +4,24 @@ import Card from './Card';
 
 function ExpenseItem(props) {
 
+  const deleteHandler = () => {
+    const expense = document.getElementById('expense');
+    expense.remove();
+    console.log('deleted !!');
+  }
+
   return (
-    <Card className='expense-item'>
-        <ExpenseDate date={props.date}/>
-        <div className='expense-item__description'>
-            <h2>{props.title}</h2>
-            <div className='expense-item__price'>${props.amount}</div>
-        </div>
-    </Card>
+    <div id='expense'>
+      <Card className='expense-item'>
+          <ExpenseDate date={props.date} />
+          <div className='expense-item__description'>
+              <h2>{props.title}</h2>
+              <div className='expense-item__price'>${props.amount}</div>
+              <button style={{borderRadius:'12px', marginLeft:'3px'}} onClick={deleteHandler}>Delete</button>
+          </div>
+      </Card>
+    </div>
   );
-}
+};
 
 export default ExpenseItem;
