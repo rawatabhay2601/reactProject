5fdaa26edb44fbc0452ce434e88ceba26bfce83f
rawatabhay2601 <rawatabhay2601@gmail.com>
2023-07-28 15:36:31 +0530

    Adding Two Way Binding and Lifting State

--- src/components/Expense/ExpenseItem.js
+++ src/components/Expense/ExpenseItem.js
@@ -1,12 +1,12 @@
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
-import Card from './Card';
+import Card from '../UI/Card';
 import { useState } from 'react';
 
 function ExpenseItem(props) {
 
   const [price, setAmount] = useState(props.amount);
-    
+
   const deleteHandler = () => {
     const expense = document.getElementById('expense');
     expense.remove();
@@ -21,13 +21,13 @@ function ExpenseItem(props) {
   return (
     <div id='expense'>
       <Card className='expense-item'>
-          <ExpenseDate date={props.date} />
-          <div className='expense-item__description'>
-              <h2>{props.title}</h2>
-              <div className='expense-item__price'>${price}</div>
-              <button style={{borderRadius:'12px', marginLeft:'2px'}} onClick={updateHandler}>Update Price</button>
-              <button style={{borderRadius:'12px', marginLeft:'2px'}} onClick={deleteHandler}>Delete</button>
-          </div>
+        <ExpenseDate date={props.date} />
+        <div className='expense-item__description'>
+          <h2>{props.title}</h2>
+          <div className='expense-item__price'>${price}</div>
+          <button style={{ borderRadius: '12px', marginLeft: '2px' }} onClick={updateHandler}>Update Price</button>
+          <button style={{ borderRadius: '12px', marginLeft: '2px' }} onClick={deleteHandler}>Delete</button>
+        </div>
       </Card>
     </div>
   )
